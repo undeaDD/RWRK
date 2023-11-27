13cbf45a71cf67f593efd85fef0e741da4bbf32e
Dominic Drees <dominic.drees@live.de>
2023-11-27 21:16:14 +0100

    test

--- assets/js/iframe.js
+++ assets/js/iframe.js
@@ -1,6 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
     window.addEventListener('message',function(event) {
-        var message = JSON.parse(event.data);
-        console.log(message.height);
+        console.log(event);
     }, false);
 });
