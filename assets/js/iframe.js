93eda6e6f042427396eaf54cadf4aeb215da49fc
Dominic Drees <dominic.drees@live.de>
2024-02-04 20:12:51 +0100

    update

--- assets/js/iframe.js
+++ assets/js/iframe.js
@@ -1,7 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
     window.addEventListener('message',function(event) {
         if (event.data) {
-            console.log(event.data);
             document.getElementById("apiFrame").style.height = event.data + "px";
         }
     }, false);
