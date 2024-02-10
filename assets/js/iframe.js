13471cdd43ab08f02de85e800ceed6d7319e01fc
DS.undeaD_D <8116188+undeaDD@users.noreply.github.com>
2024-02-10 21:01:37 +0100

    Delete assets/js/iframe.js

--- assets/js/iframe.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    window.addEventListener('message',function(event) {
-        if (event.data) {
-            document.getElementById("apiFrame").style.height = event.data + "px";
-        }
-    }, false);
-});
